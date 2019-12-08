d2e0e272d4a2f4b100f9ebca0f16ef8ecd13aad2
Stuart Presnell <stuart.presnell@gmail.com>
2019-12-08 09:57:11 +0000

    Solution to Problem 4b

--- Problem04.py
+++ Problem04.py
@@ -25,7 +25,36 @@ def non_decreasing(n):
             return False
     return True
 
+def chunk(L):
+    """Given a list, break into chunks of identical runs"""
+    chunks = []
+    p=1
+    for i in range(len(L)-1):
+        if L[i] == L[i+1]:
+            p+=1
+        else:
+            chunks.append(p)
+            p=1
+    chunks.append(p)
+    return chunks
 
 
-print(len([x for x in target_range if meets_criteria(x)]))
+def has_exact_double(n):
+    """Given a list of ints, return true if exactly two adjacent digits are the same"""
+    return 2 in chunk(n)
+
+def meets_criteria_b(n):
+    n = int_to_list_of_digits(n)
+    return has_exact_double(n) and non_decreasing(n)
+
+
+# print(
+#     has_exact_double([1,2,2,2,5,5,5,1])
+# )
+
+
+# print(len([x for x in target_range if meets_criteria(x)]))
 # Answer: 1246
+
+print(len([x for x in target_range if meets_criteria_b(x)]))
+# Answer: 814
