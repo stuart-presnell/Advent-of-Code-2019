d8e968c3dc6e8a497e8813f5c40f95c04f8b514b
Stuart Presnell <stuart.presnell@gmail.com>
2019-12-14 11:22:55 +0000

    Solution to Problem 5b

--- Problem05b.py
+++ Problem05b.py
@@ -15,14 +15,6 @@ def int_to_list_of_digits(n):
     return [int(x) for x in str(n)]
 
 
-# def process(intcode, pointer, op):
-#         [loca,locb,locc] = intcode[pointer+1:pointer+4]
-#         va = intcode[loca]
-#         vb = intcode[locb]
-#         val = op(va,vb)
-#         new_list = modify_list(intcode, locc, val)
-#         return (new_list, pointer+4)
-
 def interpret_opcode(raw):
     """Given a number, interpret it as an opcode plus a list of parameter modes:
     e.g. ABCDE = opcode DE, plus parameter modes [C, B, A]"""
@@ -39,8 +31,8 @@ def interpret_opcode(raw):
 
 def step(intcode, pointer):
     """Given an intcode program and a pointer to a position, 
-    compute one step of the program and either exit or 
-    return an updated intcode program and pointer position"""
+    compute one step of the program based on the opcode at that pointer position 
+    and either exit or return an updated intcode program and pointer position"""
     raw_opcode = intcode[pointer]
     (opcode, modes) = interpret_opcode(raw_opcode)
 
@@ -54,8 +46,16 @@ def step(intcode, pointer):
         return step_input(intcode, pointer, modes)
     elif opcode == 4:
         return step_output(intcode, pointer, modes)
+    elif opcode == 5:
+        return step_jump_if_true(intcode, pointer, modes)
+    elif opcode == 6:
+        return step_jump_if_false(intcode, pointer, modes)
+    elif opcode == 7:
+        return step_less_than(intcode, pointer, modes)
+    elif opcode == 8:
+        return step_equal(intcode, pointer, modes)
     else:
-        raise ValueError("opcode is not 1, 2, 3, 4, or 99")
+        raise ValueError("raw_opcode was " + str(raw_opcode) + " giving opcode " + str(opcode) + " which is not 1--8, or 99")
 
 def lookup_val(intcode, param, mode):
     if mode == 0:   # "position mode causes the parameter to be interpreted as a position - if the parameter is 50, its value is the value stored at address 50 in memory."
@@ -65,11 +65,6 @@ def lookup_val(intcode, param, mode):
     else:
         raise ValueError("I only understand Position mode (0) and Immediate mode (1)")
 
-# def convert_values(L, modes):
-#     args = list(zip_longest(L, modes, fillvalue=0)) 
-#         # list of pairs [(param_i, mode_i)], with default mode = 0
-#     inputs = [lookup_val(intcode, p, m) for (p,m) in args]
-#         # each param has now been interpreted according to its mode
 
 def read_values(intcode, position, x, modes):
     """Given a position in the intcode program, a list of parameter modes, and a number x of values to lookup,
@@ -78,37 +73,79 @@ def read_values(intcode, position, x, modes):
     elements_and_modes = list(zip_longest(L, modes, fillvalue=0))  # list of pairs [(elt_i, mode_i)], with default mode = 0
     return [lookup_val(intcode, e, m) for (e,m) in elements_and_modes]
 
-def step_add(intcode, pointer, modes):
+#######################################################################
+# Implementing the opcodes:
+#######################################################################
+
+def step_add(intcode, pointer, modes):  # opcode 1
     """Read the values according to modes, then return the modified program & pointer"""
-    inputs = read_values(intcode, pointer+1, 2, modes)
+    inputs = read_values(intcode, pointer+1, 2, modes)  # Read 2 inputs
     write_location = intcode[pointer+3]
     v = inputs[0] + inputs[1]
     new_list = modify_list(intcode, write_location, v)
     return (new_list, pointer+4)
 
-def step_mult(intcode, pointer, modes):
+def step_mult(intcode, pointer, modes):  # opcode 2
     """Read the values according to modes, then return the modified program & pointer"""
-    """Read the values according to modes, then return the modified program & pointer"""
-    inputs = read_values(intcode, pointer+1, 2, modes)
+    inputs = read_values(intcode, pointer+1, 2, modes)  # Read 2 inputs
     write_location = intcode[pointer+3]
     v = inputs[0] * inputs[1]
     new_list = modify_list(intcode, write_location, v)
     return (new_list, pointer+4)
 
-def step_input(intcode, pointer, modes):
+def step_input(intcode, pointer, modes):  # opcode 3
     """Read input from the user, write to location"""
     write_location = intcode[pointer+1]
     val = int(input("Input: "))
     new_list = modify_list(intcode, write_location, val)
     return (new_list, pointer+2)
 
-def step_output(intcode, pointer, modes):
+def step_output(intcode, pointer, modes):  # opcode 4
     """Print output from specified location"""
-    val = read_values(intcode, pointer+1, 1, modes)[0]
-    # location = intcode[pointer+1]
-    # val = intcode[location]
+    val = read_values(intcode, pointer+1, 1, modes)[0]  # Read 1 input
     print("Output: " + str(val))
     return (intcode, pointer+2)
+
+def step_jump_if_true(intcode, pointer, modes):  # opcode 5
+    """if the first parameter is non-zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing."""
+    inputs = read_values(intcode, pointer+1, 2, modes)  # Read 2 inputs
+    if inputs[0] == 0:
+        return (intcode, pointer+3)
+    else:
+        return (intcode, inputs[1])
+
+def step_jump_if_false(intcode, pointer, modes):  # opcode 6
+    """if the first parameter is zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing."""
+    inputs = read_values(intcode, pointer+1, 2, modes)  # Read 2 inputs
+    if inputs[0] == 0:
+        return (intcode, inputs[1])
+    else:
+        return (intcode, pointer+3)
+
+def step_less_than(intcode, pointer, modes):  # opcode 7
+    """if the first parameter is less than the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0."""
+    inputs = read_values(intcode, pointer+1, 2, modes)  # Read 2 inputs
+    write_location = intcode[pointer+3] # and a write location
+    if inputs[0] < inputs[1]:
+        val = 1
+    else:
+        val = 0
+    new_list = modify_list(intcode, write_location, val)
+    return (new_list, pointer+4)
+
+def step_equal(intcode, pointer, modes):  # opcode 8
+    """if the first parameter is equal to the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0."""
+    inputs = read_values(intcode, pointer+1, 2, modes)  # Read 2 inputs
+    write_location = intcode[pointer+3] # and a write location
+    if inputs[0] == inputs[1]:
+        val = 1
+    else:
+        val = 0
+    new_list = modify_list(intcode, write_location, val)
+    return (new_list, pointer+4)
+
+
+######################################################################
 
 def run(intcode):
     pointer = 0
@@ -146,8 +183,43 @@ def verbose_run(prog):
 #     99
 # ]
 
-# for p in [testprog0, testprog1, testprog3, testprog4]: 
-#     verbose_run(p)
+# testprog5b_1 = [
+#     3,9,
+#     8,9,10,9,
+#     4,9,99,-1,8]  #Using position mode, consider whether the input is equal to 8; output 1 (if it is) or 0 (if it is not).
+
+# testprog5b_2 = [
+#     3,9,
+#     7,9,10,9,
+#     4,9,99,-1,8]  #Using position mode, consider whether the input is less than 8; output 1 (if it is) or 0 (if it is not).
+
+# testprog5b_3 = [
+#     3,3,
+#     1108,-1,8,3,
+#     4,3,99]  #Using immediate mode, consider whether the input is equal to 8; output 1 (if it is) or 0 (if it is not).
+
+# testprog5b_4 = [
+#     3,3,
+#     1107,-1,8,3,
+#     4,3,99]  #Using immediate mode, consider whether the input is less than 8; output 1 (if it is) or 0 (if it is not).
+
+# testprog5b_5 = [
+#     3,12,     # Store input in {12}
+#     6,12,15,  # If input is zero, jump to instruction at {15}=9
+#     1,13,14,13, # Else add 1 to content of {13}
+#     4,13,99,-1,0,1,9] # take an input, then output 0 if the input was zero, or 1 if the input was non-zero:
+
+# testprog5b_6 = [
+#     3,3,     # Store input in {3}
+#     1105,-1,9,  # If input is non-zero, jump to instruction at 9
+#     1101,0,0,12, # Else write 0+0 to {12}
+#     4,12,99,1] # take an input, then output 0 if the input was zero, or 1 if the input was non-zero:
+
+# testprog5b_7 = [3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99]
+
+
+# for p in [testprog5b_7]:
+#     run(p)
 #     print()
 
 
@@ -162,48 +234,7 @@ def verbose_run(prog):
 
 ####################################
 # Problem 5b:
-# verb = 12
-# noun = 2
-
-# prob2b_base = [int(x) for x in list_from_file("Prob2-intcode.txt", sep=",")]
-
-# def input_verb_noun(prog, noun, verb):
-#     """Given a verb and a noun, input them into positions 1 and 2 of prog"""
-#     prog_v1 = modify_list(prog, 1, noun)
-#     prog_v2 = modify_list(prog_v1, 2, verb)
-#     return prog_v2
-
-# def run_AGC(noun, verb):
-#     """Run the Apollo Guidance Computer (with fixed prob2b_base)
-#     using specified verb and noun,
-#     return the final value of address 0"""
-#     prog = input_verb_noun(prob2b_base, noun, verb)
-#     output = run(prog)
-#     return output[0]
-
-# print(run_AGC(0,0))
-
-# print(run_AGC(12,2))
-
-# def find_target_value(target):
-#     for noun in range(100):
-#         for verb in range(100):
-#             if run_AGC(noun, verb) == target:
-#                 return (100 * noun + verb)
-
-# print(find_target_value(19690720))
-# Answer: 2552
-
-# Checking this answer:
-# print(run_AGC(25,52))
-
-
-# Investigating this further:
-# prog0 = input_verb_noun(prob2b_base, 12, 2)
-
-# prog1 = step(prog0,0)[0]
-# prog2 = step(prog1,4)[0]
-
-# print(prog0[:20])
-# print(prog1[:20])
-# print(prog2[:20])
+# prob5b_text = list_from_file("Prob5-input.txt", sep=",")
+# prob5b_code = [int(x) for x in prob5b_text]
+# run(prob5b_code)
+# # Answer: 5525561
